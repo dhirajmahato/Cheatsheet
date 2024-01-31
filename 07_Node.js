bd17c8ef6af592eb74ece2ab54668c2fda1a2cc8
Dhiraj Mahato <dhiraj.mahato.iitbhu@gmail.com>
2024-01-31 11:44:44 +0530

    Create 07_Node.js

---
+++ 07_Node.js
@@ -0,0 +1,9 @@
+## Node.js
+Node.js is an open-source and cross-platform JavaScript runtime environment.
+Node.js provides a set of asynchronous I/O primitives in its standard library.
+When Node.js performs an I/O operation, like reading from the network, accessing a database or the filesystem, instead of blocking the thread and wasting CPU cycles waiting, Node.js will resume the operations when the response comes back.
+This allows Node.js to handle thousands of concurrent connections with a single server without introducing the burden of managing thread concurrency.
+
+## Node.js Applications
+1. Basic HTTP Server
+2.
